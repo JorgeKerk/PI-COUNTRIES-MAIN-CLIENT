a8ccf1c2e82b873c53d98dd0b8f0870c332e068c
Jorge Kerk <jorgekerk@gmail.com>
2023-03-01 08:26:03 -0300

    Responsive

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import axios from 'axios'
 
 
 // LOCAL CONNECTION
-axios.defaults.baseURL = 'http://localhost:3001/'
+// axios.defaults.baseURL = 'http://localhost:3001/'
 
 // DEPLOYED CONNECTION
-// axios.defaults.baseURL = 'https://pi-countries-main-server-jakerk.up.railway.app/'
+axios.defaults.baseURL = 'https://pi-countries-main-server-jakerk.up.railway.app/'
 
 ReactDOM.render(
   <Provider store= { store }>
